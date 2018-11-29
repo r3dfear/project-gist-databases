b4dc122e71cfc06431b6ed1be190e531d7921daf
Khasan Musabaev <r3dfear@icloud.com>
2018-11-29 04:40:54 +0000

    Passed the tests without the opitional functionality ones in test_search.py

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,38 @@
 import requests
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    r = requests.get('https://api.github.com/users/{}/gists'.format(username))
+    r.raise_for_status()
+    
+    insert_query = '''
+    INSERT INTO gists (
+        "github_id", "html_url", "git_pull_url",
+        "git_push_url", "commits_url",
+        "forks_url", "public", "created_at",
+        "updated_at", "comments", "comments_url"
+    ) VALUES (
+        :github_id, :html_url, :git_pull_url,
+        :git_push_url, :commits_url, :forks_url,
+        :public, :created_at, :updated_at,
+        :comments, :comments_url
+    );
+    '''
+    
+    for gist in r.json():
+        params = {
+            "github_id": gist['id'],
+            "html_url": gist['html_url'],
+            "git_pull_url": gist['git_pull_url'],
+            "git_push_url": gist['git_push_url'],
+            "commits_url": gist['commits_url'],
+            "forks_url": gist['forks_url'],
+            "public": gist['public'],
+            "created_at": gist['created_at'],
+            "updated_at": gist['updated_at'],
+            "comments": gist['comments'],
+            "comments_url": gist['comments_url']
+        }
+        db.execute(insert_query, params)
+    
+    if commit:
+        db.commit()
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,15 @@
 from .models import Gist
 
 def search_gists(db_connection, **kwargs):
-    pass
+    if not kwargs:
+        query = 'SELECT * FROM gists'
+    elif 'github_id' in kwargs:
+        query = 'SELECT * FROM gists WHERE github_id = :github_id'
+    elif 'created_at' in kwargs:
+        query = 'SELECT * FROM gists WHERE datetime(created_at) == datetime(:created_at)'
+        
+    cursor = db_connection.execute(query, kwargs)
+    result = []
+    for gist in cursor:
+        result.append(Gist(gist))
+    return result
